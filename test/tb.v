ea0ee1ba71677ce166e0193466dd379768fb3f03
Alex <alexander4740@gmail.com>
2026-02-20 18:06:31 -0500

    added missing ports in convolution engine module

--- test/tb.v
+++ test/tb.v
@@ -28,13 +28,13 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  convolution_engine user_project (
 
       // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
+      `ifdef GL_TEST
+            .VPWR(VPWR),
+            .VGND(VGND),
+      `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
